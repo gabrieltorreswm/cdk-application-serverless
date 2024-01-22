859814dc2ba8e1cb27f48e0ea96e6411459003d7
gtorresp <gtorresp@bolivariano.com>
2024-01-22 10:13:22 -0500

    add: set name of stack and add queue to the deploy

--- lib/cdk-applicaiton-serverless-stack.ts
+++ lib/cdk-applicaiton-serverless-stack.ts
@@ -6,11 +6,10 @@ import * as sqs from 'aws-cdk-lib/aws-sqs';
 export class CdkApplicaitonServerlessStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-  }
-
-  const queue = new sqs.Queue(this, 'queue-process', {
-    queueName: `queue-process`,
-    visibilityTimeout: cdk.Duration.seconds(300),
-  });
-      
+    
+    const queue = new sqs.Queue(this, 'queue-process', {
+      queueName: `queue-process`,
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
+  }   
 }
